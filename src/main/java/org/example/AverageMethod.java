fff8103222841534f702a9657c980f6ba89b268b
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-05 15:59:56 -0400

    small changes

--- src/main/java/org/example/AverageMethod.java
+++ src/main/java/org/example/AverageMethod.java
@@ -4,11 +4,11 @@ public class AverageMethod {
     public static void main(String[] args){
         System.out.println((double)average(2,5,8));
     }
-    public static int sum (int num1, int num2, int num3) {
-        return num1 + num2 + num3;
+    public static int sum (int sum1, int sum2, int sum3) {
+        return sum1 + sum2 + sum3;
     }
     public static int average (int avg1, int avg2, int avg3) {
-       int solution = sum(avg1, avg2, avg3);
-       return solution/3;
+       int baseSum = sum(avg1, avg2, avg3);
+       return baseSum/3;
     }
 }
